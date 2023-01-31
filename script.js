7da27be8cfd7a247c04c6d8c546dd01a98a43e93
Bruno Souza <brunoalksouza@gmail.com>
2023-01-31 11:04:21 -0300

    sidebar zuada

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-class NavBar {
-  constructor(menu, navList, navLinks) {
-    this.Menu = document.querySelector(menu);
-    this.NavList = document.querySelector(navList);
-    this.NavLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+// Objectivo
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+// Adicionar funcionalidade de abrir e fechar a sidebar quando se clica no botão
+// Sugestão: Mudar propriedade 'display'
 
-  handleClick() {
-    this.navList.classList.toggle(this.activeClass);
-  }
+let abrir = true;
+/*
+document.querySelector('#abrir-sidebar').addEventListener('click', (e) => {
+    document.querySelector('.container').style.display = 'none';
+}, false);
 
-  addClickEvent() {
-    this.Menu.addEventListener("click", this.handleClick);
-  }
+document.querySelector('#abrir-sidebar').addEventListener('click', (e) => {
+    document.querySelector('.container').style.display = 'flex';
+}, true);*/
 
-  init() {
-    if (this.Menu) {
-      this.addClickEvent();
-    }
-    return this;
+document.querySelector('#abrir-sidebar').addEventListener('click', abrirFechar)
+function abrirFechar() {
+  if(abrir) {
+    document.querySelector('.container').style.display = 'none';
+    abrir = false;
+  } else {
+    document.querySelector('.container').style.display = 'flex';
+    abrir = true;
   }
 }
-const MenuNavBar = new NavBar(".menu", ".nav-list", ".nav-link");
+
+// Adicionar dia de hoje e hora à sidebar
+// Sugestão: usar Date
+
+const d = new Date();
+let day = d.getUTCDate();
+
+let month = (d.getUTCMonth() <= 9) ? "" + d.getUTCMonth() : d.getUTCMonth();
+let monthM = month + 1;
+let year = d.getUTCFullYear();
+let hour = d.getUTCHours();
+let minutes = d.getUTCMinutes() <= 9 ? "0" + d.getUTCMinutes() : d.getUTCMinutes();
+
+document.getElementById("dia").innerHTML = day + "/" + monthM + "/" + year
+document.getElementById("hora").innerHTML = hour + ":" + minutes
